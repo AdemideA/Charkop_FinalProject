86fde9b24633f47d7ddc78e3dd5e5c443f975a81
AdemideA <aoa307@nyu.edu>
2017-04-03 10:59:10 -0400

    update to https urls

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
       zoom: 15
     });
 
-    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
     }).addTo(map);
 
