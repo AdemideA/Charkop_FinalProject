8f19080f2e718f1fe1cb84829a22daddaadbbaf1
AdemideA <aoa307@nyu.edu>
2017-04-03 19:34:55 -0400

    attempting to fix https

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@
       zoom: 15
     });
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
+    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
     }).addTo(map);
 
     $.getJSON("data/data.geojson", function(data) { 
